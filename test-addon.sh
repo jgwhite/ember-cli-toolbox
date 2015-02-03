e9a08f0038483a5dce899ee55bab712215ac7298
Jamie White <jamie@jgwhite.co.uk>
2015-02-03 14:22:58 +0000

    Update test-addon

--- test-addon.sh
+++ test-addon.sh
@@ -12,23 +12,40 @@ else
   branch="ember-cli/master"
 fi
 
+ reset="\033[0m"
+   red="\033[0;31m"
+ green="\033[0;32m"
 yellow="\033[0;33m"
-reset="\033[0m"
 
 echo -e "\nTesting $yellow$repo$reset against $yellow$branch$reset...\n"
 
 echo -e "Removing $addon/...${reset}"
 rm -rf $addon
+
 echo -e "Cloning...${reset}"
 git clone git@github.com:$repo > /dev/null 2>&1
 cd $addon
+
 echo -e "Installing $branch..."
 npm i --save-dev $branch > /dev/null 2>&1
+
 echo -e "Installing NPM dependencies..."
 npm i > /dev/null 2>&1
+
 echo -e "Installing Bower dependencies..."
 bower i > /dev/null 2>&1
+
 echo -e "Testing..."
 npm test
+result=$?
+
 echo -e "Removing $addon/...${reset}"
 rm -rf $addon
+
+if [ "$result" -eq "0" ]; then
+  echo -e "\nResult: ${green}Passed!${reset}"
+else
+  echo -e "\nResult: ${red}Failed!${reset}"
+fi
+
+exit $result
